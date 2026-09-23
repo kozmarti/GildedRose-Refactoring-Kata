52bb95f4b22d061a9afd0ef6cffb3555e7cb4584
Marta Kozma <kozmarti@gmail.com>
2026-09-24 00:11:43 +0200

    refactor: add type hints and drop python 2 leftovers

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+from __future__ import annotations
 
 AGED_BRIE = "Aged Brie"
 BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
@@ -14,48 +14,48 @@ BACKSTAGE_TRIPLE_INCREASE_DAYS = 5
 class ItemUpdater:
     """Updates the sell_in and quality of one item at the end of a day."""
 
-    def __init__(self, item):
+    def __init__(self, item: Item) -> None:
         self.item = item
 
-    def update(self):
+    def update(self) -> None:
         self.update_quality()
         self.item.sell_in -= 1
         if self.item.sell_in < 0:
             self.update_quality_after_sell_by()
 
-    def update_quality(self):
+    def update_quality(self) -> None:
         raise NotImplementedError
 
-    def update_quality_after_sell_by(self):
+    def update_quality_after_sell_by(self) -> None:
         raise NotImplementedError
 
-    def increase_quality(self, amount):
+    def increase_quality(self, amount: int) -> None:
         if self.item.quality < MAX_QUALITY:
             self.item.quality = min(self.item.quality + amount, MAX_QUALITY)
 
-    def decrease_quality(self, amount):
+    def decrease_quality(self, amount: int) -> None:
         if self.item.quality > MIN_QUALITY:
             self.item.quality = max(self.item.quality - amount, MIN_QUALITY)
 
 
 class NormalItem(ItemUpdater):
-    def update_quality(self):
+    def update_quality(self) -> None:
         self.decrease_quality(1)
 
-    def update_quality_after_sell_by(self):
+    def update_quality_after_sell_by(self) -> None:
         self.decrease_quality(1)
 
 
 class AgedBrie(ItemUpdater):
-    def update_quality(self):
+    def update_quality(self) -> None:
         self.increase_quality(1)
 
-    def update_quality_after_sell_by(self):
+    def update_quality_after_sell_by(self) -> None:
         self.increase_quality(1)
 
 
 class BackstagePass(ItemUpdater):
-    def update_quality(self):
+    def update_quality(self) -> None:
         if self.item.sell_in <= BACKSTAGE_TRIPLE_INCREASE_DAYS:
             self.increase_quality(3)
         elif self.item.sell_in <= BACKSTAGE_DOUBLE_INCREASE_DAYS:
@@ -63,33 +63,33 @@ class BackstagePass(ItemUpdater):
         else:
             self.increase_quality(1)
 
-    def update_quality_after_sell_by(self):
+    def update_quality_after_sell_by(self) -> None:
         self.item.quality = MIN_QUALITY
 
 
 class Sulfuras(ItemUpdater):
-    def update(self):
+    def update(self) -> None:
         pass
 
 
-UPDATERS_BY_NAME = {
+UPDATERS_BY_NAME: dict[str, type[ItemUpdater]] = {
     AGED_BRIE: AgedBrie,
     BACKSTAGE_PASSES: BackstagePass,
     SULFURAS: Sulfuras,
 }
 
 
-def updater_for(item):
+def updater_for(item: Item) -> ItemUpdater:
     updater_class = UPDATERS_BY_NAME.get(item.name, NormalItem)
     return updater_class(item)
 
 
-class GildedRose(object):
+class GildedRose:
 
-    def __init__(self, items):
+    def __init__(self, items: list[Item]) -> None:
         self.items = items
 
-    def update_quality(self):
+    def update_quality(self) -> None:
         for item in self.items:
             updater_for(item).update()
 
